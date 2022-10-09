190210acb1a9b38582371497aa31db80742377c1
Banzneri <pasi.hanhijarvi@gmail.com>
2022-10-09 18:07:55 +0300

    install dayjs & add a test post

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { Post, Prisma, PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
 import express from "express";
 
 const app = express();
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.post.create({
+    data: {
+      tags: "jeejee",
+    },
+  });
+  const posts = prisma.post.findMany();
+  console.log(posts);
+}
+
+main();
 
 app.listen(3000, "localhost", () => console.log("Server listening on port 3000"));
